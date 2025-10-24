46e5328f393bf7ab4de0b426e007462387cfa57b
RileyTyler12 <coolrileytyler@gmail.com>
2025-10-23 22:27:24 -0400

    Enhance Etch-a-Sketch functionality with controls for grid size, paint intensity, and color selection

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -3,17 +3,35 @@
 
 //General Variables
 let mainContainer = document.getElementById('main-container');
+let gridSize = 16;
+let paintIntensity = "10";
+let paintColor = "#000000";
 
 //Make Grid on Start
-let gridSize = 32;
-createGrid(gridSize);
+createGrid();
+
+//add event listener to grid size button
+document.getElementById("grid-reset-button").addEventListener("click", resetGrid);
 
 //add event listener to grid size button
 document.getElementById("grid-size-button").addEventListener("click", promptSetGridSize);
 
+//add event listener and function to paint intensity
+document.getElementById("paint-intensity-slider").addEventListener('input', (event) => {
+    let inputValue = document.getElementById("paint-intensity-slider").value;
+    paintIntensity = inputValue;
+    console.log('slider activated');
+})
+//add event listener and function to paint color
+document.getElementById("paint-color-picker").addEventListener('input', (event) => {
+    let inputValue = document.getElementById("paint-color-picker").value;
+    paintColor = inputValue;
+    console.log('color picker activated')
+})
+
 //Functions
 
-function createGrid(gridSize) {
+function createGrid() {
     // Set the CSS variable for grid size
     document.documentElement.style.setProperty('--grid-size', gridSize);
     
@@ -23,20 +41,39 @@ function createGrid(gridSize) {
         div.style.opacity = "0";
         div.className = "grid-div-item";
         mainContainer.appendChild(div);
-        div.addEventListener('mouseover', (event) => {
+        div.addEventListener('pointerdown', (event) => {
             let target = event.target;
-            console.log((parseFloat(target.style.opacity) + .10) + "0");
-            target.style.opacity = (parseFloat(target.style.opacity) + .10);
+            console.log(parseFloat(target.style.opacity) + parseFloat(("." + paintIntensity))); // for debugging
+            target.style.opacity = (parseFloat(target.style.opacity) + parseFloat("." + paintIntensity));
+            target.style.backgroundColor = paintColor;
         });
     }
-    //Format Main Container
-
+    updateGridSizeText();
 }
 
 function promptSetGridSize() {
-    
+    let validInput = false;
+    let userInput = prompt("Enter new grid size from 0 to 100:");
+    while (validInput === false) {
+        //validate user input
+        let userInt = parseInt(userInput);
+        if (userInt > 0 && userInt <= 100) {
+            validInput = true;
+            gridSize = userInt;
+            resetGrid();
+        }
+        else {
+            userInput = prompt("Invalid Input. Please enter new grid size from 0 to 100:");
+        }
+    }
 }
 
-function removeGrid() {
+function resetGrid() {
     mainContainer.replaceChildren("");
+    createGrid();
+}
+
+function updateGridSizeText() {
+    let element = document.getElementById("grid-size-text");
+    element.textContent = "Current Grid Size: " + gridSize;
 }
